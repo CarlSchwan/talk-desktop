a11a79eac4da1b1e6c5723a9f8b92754b030e53d
Arthur Schiwon <blizzz@arthur-schiwon.de>
2019-11-09 01:06:04 +0100

    permanently delete old files

--- src/ncauthnamf.cpp
+++
@@ -1,8 +0,0 @@
-#include "ncauthnamf.h"
-
-QNetworkAccessManager *NcAuthNAMF::create(QObject *parent)
-{
-    qDebug() << "my QNAM!! :) ";
-    QNetworkAccessManager *nam = new NcAuthNetworkAccessManager(parent);
-    return nam;
-}
